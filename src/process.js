5c6d32423e11a6a7032ee35fbd2700ee86f75b6f
kjkj-kjkj <jasmeetkh01@gmail.com>
2025-01-26 19:52:21 +0000

    attemptng to test if JSON

--- src/process.js
+++ src/process.js
@@ -3,5 +3,14 @@ function processfn(data){
         // throw error
         throw new Error('No data');
     }
+    else {
+        try{
+            JSON.parse(data);
+            return 'is JSON';
+        }
+        catch(err){
+            return 'not a JSON'
+        }
+    }
 }
 module.exports = processfn; 
--- test/process.test.js
+++ test/process.test.js
@@ -1,6 +1,19 @@
 const processfn = require ('../src/process');
+const ex_json =  require ('../example_files/simple.json');
 
 test('errors if no data', () => { 
-   // expect(processfn(null)).toThrow('No data');
     expect(() => processfn()).toThrow('No data');
+});
+
+test('passes if receives data', () => {
+    expect(() => processfn('somedata')).isNotNull;
+});
+
+test('passes if data is not json', () => {
+    //expect(() => processfn('here have this')).toThrow();
+    expect(() => processfn('here have this').toBe('not a JSON'));
+});
+
+test('passes if data is json', () => {
+    expect(() => processfn(ex_json).toBe('not is JSON')); //this should not pass at the moment but it is passing 
 });
